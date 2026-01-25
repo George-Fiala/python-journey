2ae779a1af303247701b2e2394ef5ad95610551e
George Fiala <fialajir84@gmail.com>
2026-01-25 07:33:36 +0000

    Add dictionary filtering and tuple indexing practice

---
+++ py4e/Cisco academy/drill_00020.py
@@ -0,0 +1,15 @@
+factory_status = {}
+
+emba_1 = {
+    "Status": "Running"
+}
+
+emba_2 = {
+    "Status": "Maintenance"
+}
+
+
+factory_status.update(emba_1)
+factory_status.update(emba_2)
+
+print(factory_status)
