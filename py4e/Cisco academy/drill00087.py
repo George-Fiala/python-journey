a3248390fbb2edd85563b26f1cde0f9e246a2066
George Fiala <fialajir84@gmail.com>
2026-03-26 18:42:18 +0000

    practice

---
+++ py4e/Cisco academy/drill00087.py
@@ -0,0 +1,8 @@
+def capitalize_words(sentence):
+    words = sentence.split()
+
+    result = []
+    for word in words:
+        result.append(word.capitalize())
+       
+    return " ".join(result)
